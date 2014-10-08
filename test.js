b1afe589b7ca80429d18844f5169e177af78f6c2
vvo <vincent.voyer@gmail.com>
2014-10-08 18:16:34 +0200

    0.11 tests

--- test.js
+++ test.js
@@ -55,7 +55,7 @@ test('request, response', function(t) {
           'user-agent': userAgent,
           'connection': 'close'
         },
-        remoteAddress: '127.0.0.1',
+        remoteAddress: /^v0\.11/.test(process.version) ? '::ffff:127.0.0.1' : '127.0.0.1',
         remotePort: requestLog.req.remotePort
       }, 'request log have the right `req` property');
     });
